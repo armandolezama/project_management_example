ffe60c1280e59abb4d5e944d97e15a5b4a27a38d
armandolezama <armando.lezama.psic@gmail.com>
2022-09-05 05:57:50 -0500

    deleted unused function

--- lib/proyect_management_system_web/controllers/project_controller.ex
+++ lib/proyect_management_system_web/controllers/project_controller.ex
@@ -11,15 +11,6 @@ defmodule ProyectManagementSystemWeb.ProjectController do
     render(conn, "index.json", projects: projects)
   end
 
-  def create(conn, %{"project" => project_params}) do
-    with {:ok, %Project{} = project} <- Management.create_project(project_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.project_path(conn, :show, project))
-      |> render("show.json", project: project)
-    end
-  end
-
   def show(conn, %{"id" => id}) do
     project = Management.get_project!(id)
     render(conn, "show.json", project: project)
