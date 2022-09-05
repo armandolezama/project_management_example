8036b9a54ceec3e5d93a3fcc9dbb6bfc595ba50a
armandolezama <armando.lezama.psic@gmail.com>
2022-09-04 21:57:38 -0500

    command mix phxGenJson executed

---
+++ test/support/fixtures/management_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule ProyectManagementSystem.ManagementFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `ProyectManagementSystem.Management` context.
+  """
+
+  @doc """
+  Generate a project.
+  """
+  def project_fixture(attrs \\ %{}) do
+    {:ok, project} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        title: "some title"
+      })
+      |> ProyectManagementSystem.Management.create_project()
+
+    project
+  end
+end
